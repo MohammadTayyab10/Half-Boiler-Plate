cf23c511c721dfd76083fde9f54a596393e15a37
MohammadTayyab10 <121668781+MohammadTayyab10@users.noreply.github.com>
2023-08-16 14:55:44 +0500

    16-Aug-2023 home work

---
+++ login.js
@@ -0,0 +1,18 @@
+
+
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+
+
+
+function add(){
+// const btn = document.getElementById("btn").value;
+    console.log(email + password )
+    add()
+}
+
+
+
+// console.log(email);
+// console.log(password);
+// console.log(btn);
